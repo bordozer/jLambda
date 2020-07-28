16e8d1fcab8f4eb05d3e90b9c75694fa0c104d5f
Boris Lukianov <boris.lukianov@seniordev.com>
2020-07-28 17:43:23 +0300

    Lambda implementation 2

--- src/main/java/com/bordozer/lambda/LambdaHandler.java
+++ src/main/java/com/bordozer/lambda/LambdaHandler.java
@@ -1,8 +1,27 @@
 package com.bordozer.lambda;
 
-import lombok.extern.slf4j.Slf4j;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.bordozer.commons.utils.FileUtils;
+import com.bordozer.commons.utils.LoggableJson;
+import org.json.simple.JSONObject;
 
-@Slf4j
-public class LambdaHandler {
+import java.util.Map;
 
+public class LambdaHandler implements RequestHandler<Map<String, Object>, JSONObject> {
+
+    private static final String BODY = FileUtils.readSystemResource("fake-response.json");
+
+    @Override
+    public JSONObject handleRequest(final Map<String, Object> input, final Context context) {
+        final LambdaLogger logger = context.getLogger();
+
+        logger.log(LoggableJson.of(input).toString());
+
+        final JSONObject responseObject = new JSONObject();
+        responseObject.put("body", BODY);
+
+        return responseObject;
+    }
 }
