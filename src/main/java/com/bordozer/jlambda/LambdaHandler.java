d8239bea502167b49e31f90064c9dd8b7ff32bb5
Boris Lukianov <boris.lukianov@seniordev.com>
2020-07-29 11:23:44 +0300

    test for lambda

--- src/main/java/com/bordozer/jlambda/LambdaHandler.java
+++ src/main/java/com/bordozer/jlambda/LambdaHandler.java
@@ -3,25 +3,34 @@ package com.bordozer.jlambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.bordozer.commons.utils.FileUtils;
 import com.bordozer.commons.utils.LoggableJson;
+import com.google.common.io.Resources;
+import lombok.SneakyThrows;
 import org.json.simple.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class LambdaHandler implements RequestHandler<Map<String, String>, JSONObject> {
 
-    private static final String BODY = FileUtils.readSystemResource("fake-response.json");
-
     @Override
+    @SneakyThrows
     public JSONObject handleRequest(final Map<String, String> input, final Context context) {
         final LambdaLogger logger = context.getLogger();
 
-        logger.log(LoggableJson.of(input).toString());
+        logger.log(String.format("Input: %s", LoggableJson.of(input).toString()));
 
         final JSONObject responseObject = new JSONObject();
-        responseObject.put("body", BODY);
+        final String body = readResource();
+        responseObject.put("body", body);
 
         return responseObject;
+    }
+
+    private String readResource() throws IOException {
+        final URL url = Resources.getResource("fake-response.json");
+        return Resources.toString(url, StandardCharsets.UTF_8);
     }
 }
