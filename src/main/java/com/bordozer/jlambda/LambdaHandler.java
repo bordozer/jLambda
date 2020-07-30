414c5149b9489a9c6c5aab7a77b9884178cf5924
Boris Lukianov <boris.lukianov@seniordev.com>
2020-07-30 11:38:08 +0300

    logging

--- src/main/java/com/bordozer/jlambda/LambdaHandler.java
+++ src/main/java/com/bordozer/jlambda/LambdaHandler.java
@@ -14,19 +14,19 @@ public class LambdaHandler implements RequestHandler<Map<String, Object>, JSONOb
     public JSONObject handleRequest(final Map<String, Object> input, final Context context) {
         final LambdaLogger logger = context.getLogger();
 
-        logger.log(String.format("Input: %s", LoggableJson.of(input).toString()));
+        logger.log(String.format("Lambda input: %s", LoggableJson.of(input).toString()));
 
         final var serverUrl = System.getenv("SERVER_URL");
         final var serverPort = Integer.parseInt(System.getenv("SERVER_PORT"));
-        logger.log(String.format("Remote API: \"%s:%s\"", serverUrl, serverPort));
+        logger.log(String.format("Remote service API: \"%s:%s\"", serverUrl, serverPort));
 
         final var response = RemoteServiceHandler.get(serverUrl, serverPort);
-        logger.log(String.format("Response: %s", LoggableJson.of(response).toString()));
+        logger.log(String.format("Remote service response: %s", LoggableJson.of(response).toString()));
 
         final JSONObject responseObject = new JSONObject();
         responseObject.put("response", response);
+        logger.log(String.format("Lambda response: %s", responseObject.toJSONString()));
 
         return responseObject;
     }
-
 }
