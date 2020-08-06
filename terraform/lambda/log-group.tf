5e2fccb67926b102c96de5e903e795bb9efac2f6
Boris Lukianov <boris.lukianov@seniordev.com>
2020-08-06 20:12:07 +0300

    TF: cleanup 2

--- terraform/lambda/log-group.tf
+++ terraform/lambda/log-group.tf
@@ -1,4 +1,6 @@
+/*
 resource "aws_cloudwatch_log_group" "example" {
   name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.lambda_api_gateway.id}/${local.lambda_stage}"
   retention_in_days = 1
 }
+*/
